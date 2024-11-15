c589eca8cd40053959dd66f2cbfcb791eece58f1
Enkhchimeg Zorigt <enku1215@go.byuh.edu>
2024-11-15 12:43:57 -1000

    add formjs

---
+++ activities/engagements/form.js
@@ -0,0 +1,34 @@
+document.getElementById('myForm').addEventListener('submit',function(event) {
+    event.preventDefault();
+    const firstName = document.getElementById('firstNameInput').value;
+    const lastName = document.getElementById('lastNameInput').value;
+    if (!firstName || !lastName){
+        alert("First name and last name are required!");
+        return;
+    }
+    const formData={
+       firstNameInput: firstName,
+       lastNameInput: lastName,
+       password: document.getElementById('password').value,
+       state: document.getElementById('state').value
+    };
+    const xhr = new XMLHttpRequest();
+    xhr.open("POST" , "submit.js" , true);
+    xhr.setRequestHeader("Content Type", "application/json;charset=UTF-8")
+    xhr.onreadystatechange = function () {
+       if(xhr.readyState === 4 && xhr.status ===200) {
+          /* const response = JSON parse{xhr.responseText}*/
+           alert('Form submitted successfully.');
+       } else if(xhr.readyState === 4) {
+           alert('Error submitting form.');
+       }
+    };
+
+       xhr.send(JSON.stringify(formData));
+      alert("You submitted the form!");
+     console.log(formData)
+})
+
+    /*if (!age || age < 18) {
+        alert("You must be at least 18 to sign-up!")
+    }*/
