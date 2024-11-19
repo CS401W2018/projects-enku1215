e23ce6fbc30ad2f389198869e046fb75a37ddce1
Enkhchimeg Zorigt <enku1215@go.byuh.edu>
2024-11-18 21:07:54 -1000

    add java

--- activities/engagements/form.js
+++ activities/engagements/form.js
@@ -1,14 +1,20 @@
-document.getElementById('myForm').addEventListener('submit',function(event) {
+document.getElementById('myForm').addEventListener("submit",function(event) {
     event.preventDefault();
-    const firstName = document.getElementById('firstNameInput').value;
-    const lastName = document.getElementById('lastNameInput').value;
+    const firstName = document.getElementById("firstNameInput").value;
+    const lastName = document.getElementById("lastNameInput").value;
+    const age = document.getElementById("age").value;
     if (!firstName || !lastName){
         alert("First name and last name are required!");
+        return;}
+
+    if(!age || age < 18){
+        alert("You must be at least 18 years old to sign-up!");
         return;
     }
     const formData = {
        firstNameInput: firstName,
        lastNameInput: lastName,
+       age: age,
        password: document.getElementById('password').value,
        state: document.getElementById('state').value
     };
@@ -17,15 +23,16 @@ document.getElementById('myForm').addEventListener('submit',function(event) {
     xhr.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
     xhr.onreadystatechange = function () {
        if(xhr.readyState === 4 && xhr.status ===200) {
-          /* const response = JSON parse{xhr.responseText}*/
-           alert('Form submitted successfully.');
+         const response = JSON.parse(xhr.responseText);
+         document.getElementById("message").innerHTML = response.message;
+         document.getElementById("myForm").innerHTML = "";
        } else if(xhr.readyState === 4) {
            alert('Error submitting form.');
        }
     };
 
        xhr.send(JSON.stringify(formData));
-      alert('Success');
+      /*alert('Success');*/
       console.log(formData);
 });
 
